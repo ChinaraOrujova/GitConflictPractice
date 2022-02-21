65978c8aa2a07a747c4141e95886114dd65f80e9
timmusaka <93155060+timmusaka@users.noreply.github.com>
2022-02-21 16:27:03 -0500

    2. Created reverseString() method

--- src/JavaInterviewQuestions/StringReverse.java
+++ src/JavaInterviewQuestions/StringReverse.java
@@ -3,6 +3,21 @@ package JavaInterviewQuestions;
 public class StringReverse {
     public static void main(String[] args) {
 
+        reverseString("Cydeo");
+
     }
-    //Tim is here
+
+    public static void reverseString(String str){
+
+        String reverseStr = " ";
+
+        for (int i = str.length()-1; i >= 0; i--) {
+            reverseStr += str.charAt(i);
+
+        }
+        System.out.println(reverseStr);
+
+    }
+
+
 }
