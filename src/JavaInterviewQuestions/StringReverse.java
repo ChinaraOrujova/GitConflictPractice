aaab805bc5ddf28436339e7a3904863d884383b3
Samaritan <cihad3194@gmail.com>
2022-02-20 23:59:29 -0500

    String reverse

--- src/JavaInterviewQuestions/StringReverse.java
+++ src/JavaInterviewQuestions/StringReverse.java
@@ -1,4 +1,13 @@
 package JavaInterviewQuestions;
 
 public class StringReverse {
+    public static void main(String[] args) {
+        String str="Our group is the best";
+        String result="";
+        for (int i = str.length()-1; i >=0 ; i--) {
+            result +=str.charAt(i);
+
+        }
+        System.out.println(result);
+    }
 }
