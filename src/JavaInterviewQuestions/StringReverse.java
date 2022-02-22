7c8b6c852d5f12379e4ede33de8fa81f32b6bd6d
Kristina Stanojlovic <kristinaks93@gmail.com>
2022-02-22 18:10:51 -0500

    Kristina initial commit

--- src/JavaInterviewQuestions/StringReverse.java
+++ src/JavaInterviewQuestions/StringReverse.java
@@ -1,4 +1,12 @@
 package JavaInterviewQuestions;
 
 public class StringReverse {
+
+    public static void reverseString(String str){
+        String newStringReversed = "";
+        for (int i = str.length()-1; i >= 0; i--) {
+            newStringReversed += str.charAt(i);
+
+        }
+    }
 }
